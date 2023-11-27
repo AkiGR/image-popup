d056c1d4d82375bde242b14077402564dfcef71c
AkiGR <hanauchi.aki@gmail.com>
2023-11-27 23:31:52 +0900

    Simple version completed

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const zoom = document.querySelectorAll(".zoom")
+const zoomback = document.getElementById("zoomback")
+const zoomimg = document.getElementById("zoomimg")
+
+zoom.forEach(function(value) {
+    value.addEventListener("click",kakudai);
+});
+
+function kakudai(e) {
+    zoomback.style.display = "flex"
+    zoomimg.setAttribute("src",e.target.getAttribute("src"));
+}
+
+zoomback.addEventListener("click",modosu);
+
+function modosu() {
+    zoomback.style.display = "none";
+}
