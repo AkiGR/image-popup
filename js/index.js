2cdd394579ad26aed0e594a2d56e6bd4ad6a70c2
AkiGR <hanauchi.aki@gmail.com>
2023-11-28 00:04:57 +0900

    Pre version completed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const zoom = document.querySelectorAll(".zoom")
 const zoomback = document.getElementById("zoomback")
 const zoomimg = document.getElementById("zoomimg")
+const caption = document.getElementById("caption")
 
 zoom.forEach(function(value) {
     value.addEventListener("click",kakudai);
@@ -9,10 +10,16 @@ zoom.forEach(function(value) {
 function kakudai(e) {
     zoomback.style.display = "flex"
     zoomimg.setAttribute("src",e.target.getAttribute("src"));
+    zoomimg.classList.add("deka")
+
+    const imgCaption = e.target.getAttribute("data-caption");
+    caption.innerText = imgCaption;
 }
 
 zoomback.addEventListener("click",modosu);
 
 function modosu() {
     zoomback.style.display = "none";
+    zoomimg.classList.remove("deka")
+    caption.innerText = "";
 }
